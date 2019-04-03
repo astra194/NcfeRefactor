1635e4d0d8390c1e88a0ef4ebd32a6e3091c6515
astra194 <dave@reggub.me.uk>
2019-04-03 17:49:26 +0100

    Add unit tests to exercise existing code.

---
+++ Ncfe.CodeTest.Tests/LearnerServiceTests.cs
@@ -0,0 +1,166 @@
+﻿using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ncfe.CodeTest.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace Ncfe.CodeTest.Tests
+{
+    [TestClass]
+    public class LearnerServiceTests
+    {
+        public enum LearnerSource
+        {
+            Archive,
+            Live,
+            Failover,
+        }
+
+        private IArchivedDataService _archiveDataService;
+        private IFailoverLearnerDataAccess _failoverDataService;
+        private ILearnerDataAccess _liveDataService;
+        private IFailoverRepository _failoverRepository;
+
+        private LearnerService _sut;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _archiveDataService = A.Fake<IArchivedDataService>();
+            _failoverDataService = A.Fake<IFailoverLearnerDataAccess>();
+            _liveDataService = A.Fake<ILearnerDataAccess>();
+            _failoverRepository = A.Fake<IFailoverRepository>();
+
+            _sut = new LearnerService(_archiveDataService, _failoverDataService, _liveDataService, _failoverRepository);
+        }
+
+        [TestCleanup]
+        public void Teardown()
+        {
+            _archiveDataService = null;
+            _failoverDataService = null;
+            _liveDataService = null;
+            _failoverRepository = null;
+
+            _sut = null;
+        }
+
+        public static IEnumerable<object[]> GetLearnerAllDecisions()
+        {
+            bool[] all = new bool[] { false, true };
+
+            foreach (var isLearnerArchived in all)
+            {
+                foreach (var failoverEntriesRecent in all)
+                {
+                    foreach (var failoverEntriesThreshold in all)
+                    {
+                        foreach (var failoverModeEnabled in all)
+                        {
+                            foreach (var learnerResponseArchived in all)
+                            {
+                                yield return new object[]
+                                {
+                                      isLearnerArchived
+                                    , failoverEntriesRecent
+                                    , failoverEntriesThreshold
+                                    , failoverModeEnabled
+                                    , learnerResponseArchived
+                                };
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        [DataTestMethod, DynamicData(nameof(GetLearnerAllDecisions), DynamicDataSourceType.Method)]
+        public void GetLearnerTests(
+              bool isLearnerArchived
+            , bool failoverEntriesRecent
+            , bool failoverEntriesThreshold
+            , bool failoverModeEnabled
+            , bool learnerResponseArchived
+            )
+        {
+            var archiveLearner = new Learner();
+            A.CallTo(() => _archiveDataService.GetArchivedLearner(A<int>._)).Returns(archiveLearner);
+
+            var failoverLearner = new Learner();
+            var failoverLearnerResponse = new LearnerResponse { IsArchived = learnerResponseArchived, Learner = failoverLearner };
+            A.CallTo(() => _failoverDataService.GetLearnerById(A<int>._)).Returns(failoverLearnerResponse);
+
+            var liveLearner = new Learner();
+            var liveLearnerResponse = new LearnerResponse { IsArchived = learnerResponseArchived, Learner = liveLearner };
+            A.CallTo(() => _liveDataService.LoadLearner(A<int>._)).Returns(liveLearnerResponse);
+
+            var failoverEntries = new List<FailoverEntry>(GenerateFailoverEntries(failoverEntriesThreshold, failoverEntriesRecent));
+            A.CallTo(() => _failoverRepository.GetFailOverEntries()).Returns(failoverEntries);
+
+            ConfigurationManager.AppSettings["IsFailoverModeEnabled"] = failoverModeEnabled.ToString();
+
+            LearnerSource learnerSource = isLearnerArchived || learnerResponseArchived ? LearnerSource.Archive :
+                                            failoverEntriesRecent && failoverEntriesThreshold && failoverModeEnabled ? LearnerSource.Failover :
+                                            LearnerSource.Live;
+
+            bool archiveDataAccessed = isLearnerArchived || learnerResponseArchived;
+            bool failoverDataAccessed = !isLearnerArchived && failoverEntriesRecent && failoverEntriesThreshold && failoverModeEnabled;
+            bool liveDataAccessed = !isLearnerArchived && (!failoverEntriesRecent || !failoverEntriesThreshold || !failoverModeEnabled);
+
+            bool failoverRepositoryAccessed = !isLearnerArchived;
+
+            var learner = _sut.GetLearner(1, isLearnerArchived);
+
+            Learner expectedLearner;
+            switch (learnerSource)
+            {
+                case LearnerSource.Archive:
+                    expectedLearner = archiveLearner;
+                    break;
+
+                case LearnerSource.Failover:
+                    expectedLearner = failoverLearner;
+                    break;
+
+                case LearnerSource.Live:
+                    expectedLearner = liveLearner;
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(learnerSource));
+            }
+            Assert.AreSame(expectedLearner, learner);
+
+            if (archiveDataAccessed)
+                A.CallTo(() => _archiveDataService.GetArchivedLearner(A<int>._)).MustHaveHappenedOnceExactly();
+            else
+                A.CallTo(() => _archiveDataService.GetArchivedLearner(A<int>._)).MustNotHaveHappened();
+
+            if (failoverDataAccessed)
+                A.CallTo(() => _failoverDataService.GetLearnerById(A<int>._)).MustHaveHappenedOnceExactly();
+            else
+                A.CallTo(() => _failoverDataService.GetLearnerById(A<int>._)).MustNotHaveHappened();
+
+            if (liveDataAccessed)
+                A.CallTo(() => _liveDataService.LoadLearner(A<int>._)).MustHaveHappenedOnceExactly();
+            else
+                A.CallTo(() => _liveDataService.LoadLearner(A<int>._)).MustNotHaveHappened();
+
+            if (failoverRepositoryAccessed)
+                A.CallTo(() => _failoverRepository.GetFailOverEntries()).MustHaveHappenedOnceExactly();
+            else
+                A.CallTo(() => _failoverRepository.GetFailOverEntries()).MustNotHaveHappened();
+        }
+
+        private IEnumerable<FailoverEntry> GenerateFailoverEntries(bool failoverEntriesThreshold, bool failoverEntriesRecent)
+        {
+            int failoverEntriesCount = failoverEntriesThreshold ? 110 : 90;
+            for (int i = 0; i < failoverEntriesCount; i++)
+            {
+                DateTime entryTime = failoverEntriesRecent ? DateTime.Now : DateTime.Now - TimeSpan.FromMinutes(30);
+                yield return new FailoverEntry { DateTime = entryTime };
+            }
+        }
+    }
+}
---
+++ Ncfe.CodeTest.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ncfe.CodeTest.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ncfe.CodeTest.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b76d71e5-87fd-4bac-ae94-7a82681bca39")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
