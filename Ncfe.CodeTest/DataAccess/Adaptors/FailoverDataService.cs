36fd87882970b688a88d3db3aef138f402aafcbb
astra194 <dave@reggub.me.uk>
2019-04-04 11:35:18 +0100

    These are feeling more like adaptors than wrappers
    since they change method names etc.

--- Ncfe.CodeTest/DataAccess/Adaptors/FailoverDataService.cs
+++ Ncfe.CodeTest/DataAccess/Adaptors/FailoverDataService.cs
@@ -1,7 +1,7 @@
 ﻿using Ncfe.CodeTest.DataAccess.Actuals;
 using Ncfe.CodeTest.DataAccess.Interfaces;
 
-namespace Ncfe.CodeTest.DataAccess.Proxies
+namespace Ncfe.CodeTest.DataAccess.Adaptors
 {
     public class FailoverDataService : ILearnerDataService
     {
--- Ncfe.CodeTest/DataAccess/Adaptors/LiveDataService.cs
+++ Ncfe.CodeTest/DataAccess/Adaptors/LiveDataService.cs
@@ -1,7 +1,7 @@
 ﻿using Ncfe.CodeTest.DataAccess.Actuals;
 using Ncfe.CodeTest.DataAccess.Interfaces;
 
-namespace Ncfe.CodeTest.DataAccess.Wrappers
+namespace Ncfe.CodeTest.DataAccess.Adaptors
 {
     public class LiveDataService : ILearnerDataService
     {
